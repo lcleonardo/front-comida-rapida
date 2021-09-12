25395ea20682de4697f24b2fb24d939a3f7a7e1b
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-09-11 23:25:30 -0500

    Primera prueba del servicio DescuentoService, probando el metodo consultar, usando un Spy Object de tipo HttpService

--- src/app/feature/descuento/shared/service/descuento.service.spec.ts
+++ src/app/feature/descuento/shared/service/descuento.service.spec.ts
@@ -2,12 +2,15 @@ import { TestBed } from "@angular/core/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { DescuentoService } from "./descuento.service";
 import { HttpService } from "@core/services/http.service";
+import { of } from "rxjs";
+import { Descuento } from "../model/descuento";
 
 fdescribe("Test servicio descuento", () => {
-  let descuentoService;
+  let descuentoService: DescuentoService;
   let httpServiceSpy;
   beforeEach(async () => {
     httpServiceSpy = jasmine.createSpyObj("HttpService", [
+      "optsName",
       "doGet",
       "doPost",
       "doDelete",
@@ -22,7 +25,20 @@ fdescribe("Test servicio descuento", () => {
     descuentoService = TestBed.inject(DescuentoService);
   });
 
-  it("should be initialized", () => {
+  it("should be initialized", async () => {
     expect(descuentoService).toBeTruthy;
+  });
+
+  it("deberia consulatr pedidos", async () => {
+    //Arrange
+    let descuentos: Descuento[] = [];
+    let descuento: Descuento = new Descuento("2021,09-01", 10);
+    //Act
+    httpServiceSpy.doGet.and.returnValue(of([descuento]));
+    descuentoService.consultar().subscribe((res) => (descuentos = res));
+    //Assert
+    expect(descuentos.length).toBe(1);
+    expect(descuentos[0].fecha).toEqual("2021,09-01");
+    expect(descuentos[0].porcentaje).toEqual(10);
   });
 });
