12578b796f831d7428333246970a26cd099f9a1a
leonardo.cruz <leonardo.cruz@ceiba.com.co>
2021-09-16 13:33:42 -0500

    Creando primeros methodos de test

--- src/app/feature/descuento/components/crear-descuento/crear-descuento.component.spec.ts
+++ src/app/feature/descuento/components/crear-descuento/crear-descuento.component.spec.ts
@@ -1,11 +1,12 @@
 import { DatePipe } from '@angular/common'
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { MatDialogRef } from '@angular/material/dialog'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { DescuentoService } from '@descuento/shared/service/descuento.service'
 import { CrearDescuentoComponent } from './crear-descuento.component'
 
 fdescribe('Test crear descuento CrearDescuentoComponent', () => {
+  let fixture: ComponentFixture<CrearDescuentoComponent>
   let componente: CrearDescuentoComponent
   let descuentoServiceSpy: DescuentoService
   let datePipeSpy: DatePipe
@@ -22,10 +23,29 @@ fdescribe('Test crear descuento CrearDescuentoComponent', () => {
         { provide: MatSnackBar, useValue: matSnackBarSpy },
       ],
     }).compileComponents()
-    componente = TestBed.inject(CrearDescuentoComponent)
+    fixture = TestBed.createComponent(CrearDescuentoComponent)
+    componente = fixture.componentInstance
+    fixture.detectChanges()
   })
 
   it('should be initialized', async () => {
     expect(componente).toBeTruthy
+  })
+
+  it('deberia retornar un formulario valido', async () => {
+    let fecha = componente.formulario.get('fecha')
+    fecha.setValue(new Date().toISOString())
+    let porcentaje = componente.formulario.get('porcentaje')
+    porcentaje.setValue(10)
+    expect(componente.formulario.valid).toBe(true)
+  })
+
+  it('deberia retornar un formulario invalido, porque la fecha no puede ser menor a la fecha actual', async () => {
+    let fecha = componente.formulario.get('fecha')
+    await fecha.setValue('2021-09-15')
+    let porcentaje = componente.formulario.get('porcentaje')
+    await porcentaje.setValue(10)
+    componente.formulario.markAllAsTouched()
+    expect(componente.formulario.invalid).toBe(true)
   })
 })
